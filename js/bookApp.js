4f39b172b17c005b381eb0190aa155264560ae00
Ulises Largaespada <ulisesedu7@users.noreply.github.com>
2022-05-25 11:16:00 -0600

    Add functions to visibility in js

--- js/bookApp.js
+++ js/bookApp.js
@@ -86,12 +86,36 @@ addBook.addEventListener('click', StorageBooks.addLi);
 /*
 Activate and Deactivate Sections
 */
+allBooksSection.classList.add('active'); 
+allBooksSection.classList.remove('hide-class')
+
+function toggleVisbility(activeEle) {
+  activeEle.classList.toggle('active');
+  activeEle.classList.remove('hide-class');
+};
+
+function clearClasses(element1, element2) {
+  element1.classList.remove('active');
+  element1.classList.add('hide-class');
+
+  element2.classList.remove('active');
+  element2.classList.add('hide-class');
+};
+
 listLink.addEventListener('click', () => {
-  allBooksSection.classList.toggle('active');
-  allBooksSection.classList.remove('hide-class');
+  toggleVisbility(allBooksSection);
+
+  clearClasses(addNewBookSection, contactSection);
 });
 
 addLink.addEventListener('click', () => {
-  addNewBookSection.classList.toggle('active');
-  addNewBookSection.classList.remove('hide-class');
+  toggleVisbility(addNewBookSection);
+
+  clearClasses(allBooksSection, contactSection);
+});
+
+contactLink.addEventListener('click', () => {
+  toggleVisbility(contactSection);
+
+  clearClasses(allBooksSection, addNewBookSection);
 });
